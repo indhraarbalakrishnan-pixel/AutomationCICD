1d847a240bfabcb0ea311f8aa6488e248c5e3613
indhraa <indhraarbalakrishnan@gmail.com>
2026-03-30 17:34:41 +0530

    remove target

---
+++ src/test/java/rahulshettyacademy/tests/Gittest.java
@@ -0,0 +1,26 @@
+package rahulshettyacademy.tests;
+
+import org.testng.annotations.Test;
+
+public class Gittest {
+
+	@Test
+	public void gittesting()
+	{
+		System.out.println("print1");
+		System.out.println("print2");
+		System.out.println("print3");
+	}
+	
+<<<<<<< HEAD
+	@Test
+	public void gitesting()
+	{
+		System.out.println("print4");
+		System.out.println("print5");
+		System.out.println("print6");
+	}
+	
+=======
+>>>>>>> 4abbb94f02bc34818a26e774a8a1a46a19947f14
+}
